5114da169c693ff00f5fa3ba4b64a232dc85e95f
hunglv1 <vanhung1512@gmail.com>
2023-06-19 15:43:01 +0200

    update code
    In this commit:
    - add response for sign in API

--- src/main/resources/static/france_volunteer.sql
+++ src/main/resources/static/france_volunteer.sql
@@ -1,8 +1,10 @@
-CREATE DATABASE IF NOT EXISTS francevolunteerdb;
+CREATE
+DATABASE IF NOT EXISTS francevolunteerdb;
 
-CREATE TABLE users (
-                       user_id int NOT NULL AUTO_INCREMENT,
-                       email varchar(30) NOT NULL,
-                       password varchar(300) NOT NULL,
-                       PRIMARY KEY (user_id)
+CREATE TABLE users
+(
+    user_id  int          NOT NULL AUTO_INCREMENT,
+    email    varchar(30)  NOT NULL,
+    password varchar(300) NOT NULL,
+    PRIMARY KEY (user_id)
 );
