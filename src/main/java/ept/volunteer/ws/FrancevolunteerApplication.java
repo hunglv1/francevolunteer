6278e5612337719062573c0034168bacd46acc81
hunglv1 <vanhung1512@gmail.com>
2023-07-26 16:54:14 +0200

    update code

--- src/main/java/ept/volunteer/ws/FrancevolunteerApplication.java
+++ src/main/java/ept/volunteer/ws/FrancevolunteerApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//@EnableEncryptableProperties
-//@EnableMongoRepositories(basePackages = "ept.volunteer.ws.responsitory")
 public class FranceVolunteerApplication {
 
     public static void main(String[] args) {
